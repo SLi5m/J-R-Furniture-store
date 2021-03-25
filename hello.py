b52834e17878856ba6387eee84f5d419a6d2f64d
Sli5m <rogersbag@gmail.com>
2021-03-25 17:06:45 +0300

    routes to the hello python file

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello Wide world"
+def index():
+    return render_template('index.html')
+
+@app.route('/showroom/')
+def showroom():
+    return render_template('showroom.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
